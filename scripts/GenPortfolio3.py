fix formula

scripts/GenPortfolio3.py
@@ -36,7 +36,7 @@ tickers = ['BRK-B','LIT','ARKK','DBC','REET', 'NVDA', 'MSFT','AMZN', 'TSLA', 'JP
 apiToken = 'b6aa06a239545aa707fc32cf7ffa17f3d828380f'
 for i in tickers:
     print(i)
-    tmp = pdr.get_data_tiingo(symbols=i,start='1/1/2019', end=dt.date.today(), retry_count=5, api_key=apiToken)
+    tmp = pdr.get_data_tiingo(symbols=i,start='1/1/2021', end=dt.date.today(), retry_count=5, api_key=apiToken)
     tmp.reset_index('symbol', inplace=True, drop=True)
     Closeprice[i] = tmp['adjClose']
 
@@ -44,21 +44,43 @@ for i in tickers:
 ##returns is a dataframe class
 returns = np.log(Closeprice / Closeprice.shift(1))
 
+mean_1 = np.exp(returns.mean() * no_of_days)
+# print("mean_1")
+# print(mean_1)
+log_var = returns.var(skipna=True) * no_of_days
+# print("log_var")
+# print(log_var)
+diff = np.exp(-1*np.sqrt(log_var))
+std = np.subtract(diff*mean_1, mean_1)
+# print("std")
+# print(std)
+var = pd.DataFrame()
+for i in range(0, std.size):
+    scalar = std[i]
+    ds = std*scalar
+    var[tickers[i]] = ds
+
+# print("var")
+# print(var)
+corr = returns.corr()
+
+# print("corr")
+# print(corr)
+cov = corr*var
+
+# print("cov")
+# print(cov)
+mean = np.subtract(mean_1,1)
 
 def get_ret_vol_sr(weights):
     weights = np.array(weights)
-    mean = np.exp(returns.mean() * no_of_days)
-    # print(mean)
-    diff = np.exp(-1*np.sqrt(returns.cov() * no_of_days))
-    std = np.subtract(np.multiply(mean, diff), mean)
-    cov = np.power(std, 2)
-    # print(cov)
-
-    mean = np.subtract(mean,1)
-
+    # print("weights=", weights)
     ret = np.sum(mean * weights)
+    # print("ret=",ret)
     vol = np.sqrt(np.dot(weights.T, np.dot(cov, weights)))
+    # print("vol=",vol)
     sr = (ret - single_period_margin_rate) / vol
+    # print("sr=",sr)
     return np.array([ret, vol, sr])
 
 
@@ -80,9 +102,11 @@ def check_sum(weights):
 cons = ({'type': 'eq', 'fun': check_sum})
 bounds = [[0] * 2] * len(tickers)
 bounds[0][1] = 1
-print(bounds)
-init_guess = [0.25] * len(tickers)
-print(init_guess)
+# print(bounds)
+equal_size = 1 / len(tickers)
+print("equal_size=", equal_size)
+init_guess = [equal_size] * len(tickers)
+print("init_guess=", init_guess)
 
 opt_result = minimize(neg_sharpe, init_guess, method='SLSQP', bounds=bounds, constraints=cons)
 print(opt_result)
@@ -98,7 +122,7 @@ for i in tickers:
 
 df = pd.DataFrame(df, index=[0]).transpose()
 print(df)
-df.to_csv(r'optimal.csv', index=True, header=True)
+df.to_csv(r'ui/output/optimal.csv', index=True, header=True)
 
 # We want the key x from the dictionary, which is an array with the weights of the portfolio that has the maximum Sharpe ratio.
 # If we use our function get_ret_vol_sr we get the return, volatility, and sharpe ratio: